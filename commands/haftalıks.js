a1ae140272b75b3a82170c364cad69c84c04a7cc
owomed <owomedsunucu@gmail.com>
2025-07-25 19:01:28 +0300

    haftalıks.js

--- commands/haftalıks.js
+++ commands/haftalıks.js
@@ -1,19 +1,24 @@
 const { MessageEmbed } = require('discord.js');
-const { QuickDB } = require('quick.db');
+const Enmap = require('enmap');
 
-// Yeni quick.db API'sini kullanmak için db objesini yeniden oluştur
-const db = new QuickDB();
+// 'kayitlar' isminde yeni bir enmap veritabanı oluşturuyoruz
+const db = new Enmap({ name: "kayitlar" });
 
 module.exports = {
     name: 'haftalık-sıralama',
     aliases: ['weekly-top'],
     description: 'Haftalık kayıt sıralamasını gösterir.',
     async execute(client, message, args) {
-        // Yeni API'de tüm verileri almak için .all() metodunu kullanıyoruz.
-        const allData = await db.all();
+        // Enmap'in tüm verileri döndürmesi için `fetchEverything()` metodu kullanılır
+        const allData = await db.fetchEverything();
         
-        const weeklyData = allData.filter(data => data.id.startsWith('weekly_'));
-        const sortedData = weeklyData.sort((a, b) => b.value - a.value);
+        // Enmap verisi bir Map nesnesi olduğundan, filtreleme yapmak için Array'e dönüştürmek gerekir
+        // .filter() metodu ile haftalık kayıtları alıyoruz.
+        const weeklyData = allData.filter((value, key) => key.startsWith('weekly_'));
+        
+        // Sıralama işlemi Map üzerindeki verilerle yapılır
+        // Map'i önce Array'e dönüştürüp sonra sıralıyoruz
+        const sortedData = [...weeklyData.entries()].sort(([, a], [, b]) => b - a);
 
         let embed = new MessageEmbed()
             .setColor('#0099ff')
@@ -23,13 +28,14 @@ module.exports = {
             .setTimestamp();
 
         for (let i = 0; i < sortedData.length; i++) {
-            const userId = sortedData[i].id.split('_')[1];
+            const userId = sortedData[i][0].split('_')[1];
             let user = await client.users.fetch(userId).catch(() => null);
             const userTag = user ? user.tag : 'Bilinmeyen Kullanıcı';
-            
+            const kayitSayisi = sortedData[i][1];
+
             embed.addField(
                 `${i + 1}. ${userTag}`,
-                `Kayıt Sayısı: \`${sortedData[i].value}\`\n <:med_kivircikok:1246364420896985119> ${user ? `<@${userId}>` : 'Bilinmeyen Etiket'}`,
+                `Kayıt Sayısı: \`${kayitSayisi}\`\n <:med_kivircikok:1246364420896985119> ${user ? `<@${userId}>` : 'Bilinmeyen Etiket'}`,
                 false
             );
         }
