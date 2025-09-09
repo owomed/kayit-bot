a2001cd9735fb97367629efbee2a5bcc93f416c1
owomed <owomedsunucu@gmail.com>
2025-09-09 12:15:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ client.on('ready', async () => {
     console.log(`✅ Bot başarılı bir şekilde ${client.user.tag} olarak giriş yaptı!`);
 
     const { REST, Routes } = require('discord.js');
-    const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+    const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
 
     try {
         const guildId = process.env.GUILD_ID;
